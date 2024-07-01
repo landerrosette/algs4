ffde16b4a7defeb60ed17f2891bcd01cdb4e14fc
landerrosette <57791410+landerrosette@users.noreply.github.com>
2024-07-01 15:58:34 +0800

    separate newline characters

--- main.cpp
+++ main.cpp
@@ -22,51 +22,51 @@ int main(int argc, char *argv[]) {
     if (dataFileName == "words3.txt") {
         // 测试排序算法
         // % algs4 words3.txt
-        std::cout << "Testing selection sort\n";
-        std::cout << "--------------------------------\n";
+        std::cout << "Testing selection sort" << "\n";
+        std::cout << "--------------------------------" << "\n";
         testSort<Selection>(dataFilePath);
-        std::cout << "--------------------------------\n\n";
-        std::cout << "Testing insertion sort\n";
-        std::cout << "--------------------------------\n";
+        std::cout << "--------------------------------" << "\n" << "\n";
+        std::cout << "Testing insertion sort" << "\n";
+        std::cout << "--------------------------------" << "\n";
         testSort<Insertion>(dataFilePath);
-        std::cout << "--------------------------------\n\n";
-        std::cout << "Testing shellsort\n";
-        std::cout << "--------------------------------\n";
+        std::cout << "--------------------------------" << "\n" << "\n";
+        std::cout << "Testing shellsort" << "\n";
+        std::cout << "--------------------------------" << "\n";
         testSort<Shell>(dataFilePath);
-        std::cout << "--------------------------------\n\n";
-        std::cout << "Testing mergesort\n";
-        std::cout << "--------------------------------\n";
+        std::cout << "--------------------------------" << "\n" << "\n";
+        std::cout << "Testing mergesort" << "\n";
+        std::cout << "--------------------------------" << "\n";
         testSort<Merge>(dataFilePath);
-        std::cout << "--------------------------------\n\n";
-        std::cout << "Testing bottom-up mergesort\n";
-        std::cout << "--------------------------------\n";
+        std::cout << "--------------------------------" << "\n" << "\n";
+        std::cout << "Testing bottom-up mergesort" << "\n";
+        std::cout << "--------------------------------" << "\n";
         testSort<MergeBU>(dataFilePath);
-        std::cout << "--------------------------------\n\n";
-        std::cout << "Testing quicksort\n";
-        std::cout << "--------------------------------\n";
+        std::cout << "--------------------------------" << "\n" << "\n";
+        std::cout << "Testing quicksort" << "\n";
+        std::cout << "--------------------------------" << "\n";
         testSort<Quick>(dataFilePath);
-        std::cout << "--------------------------------\n\n";
-        std::cout << "Testing 3-way quicksort\n";
-        std::cout << "--------------------------------\n";
+        std::cout << "--------------------------------" << "\n" << "\n";
+        std::cout << "Testing 3-way quicksort" << "\n";
+        std::cout << "--------------------------------" << "\n";
         testSort<Quick3way>(dataFilePath);
-        std::cout << "--------------------------------\n\n";
-        std::cout << "Testing heapsort\n";
-        std::cout << "--------------------------------\n";
+        std::cout << "--------------------------------" << "\n" << "\n";
+        std::cout << "Testing heapsort" << "\n";
+        std::cout << "--------------------------------" << "\n";
         testSort<Heap>(dataFilePath);
-        std::cout << "--------------------------------\n\n";
+        std::cout << "--------------------------------" << "\n" << "\n";
     } else if (dataFileName == "tinyPQ.txt") {
         // 测试优先队列
         // % algs4 tinyPQ.txt
-        std::cout << "Testing priority queue\n";
-        std::cout << "--------------------------------\n";
+        std::cout << "Testing priority queue" << "\n";
+        std::cout << "--------------------------------" << "\n";
         testPQ(MaxPQ<std::string>(10), dataFilePath);
-        std::cout << "--------------------------------\n\n";
+        std::cout << "--------------------------------" << "\n" << "\n";
     } else if (dataFileName == "tinyST.txt") {
         // 测试搜索算法
         // % algs4 tinyST.txt
-        std::cout << "Testing sequential search symbol table\n";
+        std::cout << "Testing sequential search symbol table" << "\n";
         testBasicST(SequentialSearchST<std::string, int>(), dataFilePath);
-        std::cout << "Testing binary search symbol table\n";
+        std::cout << "Testing binary search symbol table" << "\n";
         testOrderedST(BinarySearchST<std::string, int>(20), dataFilePath);
     }
     return 0;
--- tests/testST.cpp
+++ tests/testST.cpp
@@ -15,35 +15,35 @@ void testBasicST(ST<std::string, int> &&st, const std::filesystem::path &dataFil
     std::cout << "size = " << st.size() << "\n";
 
     // print keys using getKeys()
-    std::cout << "Testing getKeys()\n";
-    std::cout << "--------------------------------\n";
+    std::cout << "Testing getKeys()" << "\n";
+    std::cout << "--------------------------------" << "\n";
     for (const auto &s: st.getKeys()) {
         std::cout << s << " " << st.get(s) << "\n";
     }
-    std::cout << "--------------------------------\n\n";
+    std::cout << "--------------------------------" << "\n" << "\n";
 
     // remove the first few keys
     for (int i = 0; i < st.size() / 2; ++i) {
         st.remove(st.getKeys()[0]);
     }
     std::cout << "After removing the first " << st.size() / 2 << " keys" << "\n";
-    std::cout << "--------------------------------\n";
+    std::cout << "--------------------------------" << "\n";
     for (const auto &s: st.getKeys()) {
         std::cout << s << " " << st.get(s) << "\n";
     }
-    std::cout << "--------------------------------\n\n";
+    std::cout << "--------------------------------" << "\n" << "\n";
 
     // remove all the remaining keys
     while (!st.isEmpty()) {
         std::cout << "Removing " << st.getKeys()[st.size() / 2] << "\n";
         st.remove(st.getKeys()[st.size() / 2]);
     }
-    std::cout << "After removing the remaining keys\n";
-    std::cout << "--------------------------------\n";
+    std::cout << "After removing the remaining keys" << "\n";
+    std::cout << "--------------------------------" << "\n";
     for (const auto &s: st.getKeys()) {
         std::cout << s << " " << st.get(s) << "\n";
     }
-    std::cout << "--------------------------------\n\n";
+    std::cout << "--------------------------------" << "\n" << "\n";
 }
 
 void testOrderedST(OrderedST<std::string, int> &&st, const std::filesystem::path &dataFilePath) {
@@ -60,24 +60,24 @@ void testOrderedST(OrderedST<std::string, int> &&st, const std::filesystem::path
     std::cout << "max = " << st.max() << "\n";
 
     // print keys in order using getKeys()
-    std::cout << "Testing getKeys()\n";
-    std::cout << "--------------------------------\n";
+    std::cout << "Testing getKeys()" << "\n";
+    std::cout << "--------------------------------" << "\n";
     for (const auto &s: st.getKeys()) {
         std::cout << s << " " << st.get(s) << "\n";
     }
-    std::cout << "--------------------------------\n\n";
+    std::cout << "--------------------------------" << "\n" << "\n";
 
     // print keys in order using select
-    std::cout << "Testing select()\n";
-    std::cout << "--------------------------------\n";
+    std::cout << "Testing select()" << "\n";
+    std::cout << "--------------------------------" << "\n";
     for (int i = 0; i < st.size(); ++i) {
         std::cout << i << " " << st.select(i) << "\n";
     }
-    std::cout << "--------------------------------\n\n";
+    std::cout << "--------------------------------" << "\n" << "\n";
 
     // test rank, floor, ceiling
-    std::cout << "key rank floor ceil\n";
-    std::cout << "-------------------\n";
+    std::cout << "key rank floor ceil" << "\n";
+    std::cout << "-------------------" << "\n";
     for (char i = 'A'; i <= 'Z'; ++i) {
         std::string s(1, i);
         std::cout << std::setw(2) << s << " "
@@ -85,13 +85,13 @@ void testOrderedST(OrderedST<std::string, int> &&st, const std::filesystem::path
                   << std::setw(4) << st.floor(s) << " "
                   << std::setw(4) << st.ceiling(s) << "\n";
     }
-    std::cout << "-------------------\n\n";
+    std::cout << "-------------------" << "\n" << "\n";
 
     // test range search and range count
     std::vector<std::string> from = {"A", "Z", "X", "0", "B", "C"};
     std::vector<std::string> to = {"Z", "A", "X", "Z", "G", "L"};
-    std::cout << "range search\n";
-    std::cout << "-------------------\n";
+    std::cout << "range search" << "\n";
+    std::cout << "-------------------" << "\n";
     for (int i = 0; i < from.size(); ++i) {
         std::cout << from[i] << "-" << to[i] << " ("
                   << std::setw(2) << st.size(from[i], to[i]) << ") : ";
@@ -100,28 +100,28 @@ void testOrderedST(OrderedST<std::string, int> &&st, const std::filesystem::path
         }
         std::cout << "\n";
     }
-    std::cout << "-------------------\n\n";
+    std::cout << "-------------------" << "\n" << "\n";
 
     // remove the smallest keys
     for (int i = 0; i < st.size() / 2; ++i) {
         st.removeMin();
     }
     std::cout << "After removing the smallest " << st.size() / 2 << " keys" << "\n";
-    std::cout << "--------------------------------\n";
+    std::cout << "--------------------------------" << "\n";
     for (const auto &s: st.getKeys()) {
         std::cout << s << " " << st.get(s) << "\n";
     }
-    std::cout << "--------------------------------\n\n";
+    std::cout << "--------------------------------" << "\n" << "\n";
 
     // remove all the remaining keys
     while (!st.isEmpty()) {
         std::cout << "Removing " << st.select(st.size() / 2) << "\n";
         st.remove(st.select(st.size() / 2));
     }
-    std::cout << "After removing the remaining keys\n";
-    std::cout << "--------------------------------\n";
+    std::cout << "After removing the remaining keys" << "\n";
+    std::cout << "--------------------------------" << "\n";
     for (const auto &s: st.getKeys()) {
         std::cout << s << " " << st.get(s) << "\n";
     }
-    std::cout << "--------------------------------\n\n";
+    std::cout << "--------------------------------" << "\n" << "\n";
 }
