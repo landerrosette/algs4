24649fd5a4c991fdf2523e6c60d7d973721e939b
landerrosette <57791410+landerrosette@users.noreply.github.com>
2024-12-19 03:49:17 +0800

    Add binary dump utility

---
+++ main_BinaryDump.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "BinaryStdIO.h"
+#include <string>
+
+int main(int argc, char *argv[]) {
+    int width = std::stoi(argv[1]), cnt;
+    for (cnt = 0; !BinaryStdIO::isEmpty(); ++cnt) {
+        if (width == 0) {
+            BinaryStdIO::readBool();
+            continue;
+        }
+        if (cnt != 0 && cnt % width == 0) std::cout << std::endl;
+        if (BinaryStdIO::readBool()) std::cout << "1";
+        else std::cout << "0";
+    }
+    std::cout << std::endl;
+    std::cout << cnt << " bits" << std::endl;
+    return 0;
+}
