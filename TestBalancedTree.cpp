f8b4ffe3b28ae9119fc1eb3677001455f9649d8b
landerrosette <57791410+landerrosette@users.noreply.github.com>
2024-12-26 08:57:04 +0800

    Add tests for Red-Black BST to validate tree balance

---
+++ TestBalancedTree.cpp
@@ -0,0 +1,29 @@
+#include "TestBalancedTree.h"
+#include <random>
+
+void TestST::testRedBlackBST(RedBlackBST<int, int> &st, int n, std::ostream &os) {
+    // Insert N elements in order.
+    os << "Inserting " << n << " elements in order" << std::endl;
+    os << "--------------------------------" << std::endl;
+    for (int i = 0; i < n; ++i) {
+        st.put(i, i);
+        os << "i = " << i << ", height = " << st.height() << ", size = " << st.size() << std::endl;
+    }
+    os << std::endl;
+
+    // Delete keys in random order.
+    os << "Deleting keys in random order" << std::endl;
+    os << "--------------------------------" << std::endl;
+    std::default_random_engine e(std::random_device{}());
+    std::uniform_int_distribution u(0, n - 1);
+    while (st.size() > 0) {
+        int i = u(e);
+        if (st.contains(i)) {
+            st.remove(i);
+            os << "i = " << i << ", height = " << st.height() << ", size = " << st.size() << std::endl;
+        }
+    }
+    os << std::endl;
+
+    os << "size = " << st.size() << std::endl;
+}
---
+++ TestBalancedTree.h
@@ -0,0 +1,12 @@
+#ifndef TESTBALANCEDTREE_H
+#define TESTBALANCEDTREE_H
+
+
+#include <iostream>
+#include "RedBlackBST.h"
+
+namespace TestST {
+    void testRedBlackBST(RedBlackBST<int, int> &st, int n, std::ostream &os);
+}
+
+#endif //TESTBALANCEDTREE_H
