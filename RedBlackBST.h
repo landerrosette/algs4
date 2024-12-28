c362b7196ff6751dcd31ecea840a699801358cbd
landerrosette <57791410+landerrosette@users.noreply.github.com>
2024-12-28 09:22:35 +0800

    Pass unique_ptr by reference to avoid unnecessary moves

--- RedBlackBST.h
+++ RedBlackBST.h
@@ -17,13 +17,13 @@ private:
 
     bool isRed(typename BST<Key, Value>::Node *x) const;
 
-    std::unique_ptr<typename BST<Key, Value>::Node> rotateLeft(std::unique_ptr<typename BST<Key, Value>::Node> h);
+    std::unique_ptr<typename BST<Key, Value>::Node> rotateLeft(std::unique_ptr<typename BST<Key, Value>::Node> &h);
 
-    std::unique_ptr<typename BST<Key, Value>::Node> rotateRight(std::unique_ptr<typename BST<Key, Value>::Node> h);
+    std::unique_ptr<typename BST<Key, Value>::Node> rotateRight(std::unique_ptr<typename BST<Key, Value>::Node> &h);
 
     void flipColors(typename BST<Key, Value>::Node *h);
 
-    std::unique_ptr<typename BST<Key, Value>::Node> put(std::unique_ptr<typename BST<Key, Value>::Node> h,
+    std::unique_ptr<typename BST<Key, Value>::Node> put(std::unique_ptr<typename BST<Key, Value>::Node> &h,
                                                         const Key &key, const Value &val);
 
     int height(typename BST<Key, Value>::Node *h) const;
@@ -42,7 +42,7 @@ bool RedBlackBST<Key, Value>::isRed(typename BST<Key, Value>::Node *x) const {
 
 template<typename Key, typename Value>
 std::unique_ptr<typename BST<Key, Value>::Node> RedBlackBST<Key, Value>::rotateLeft(
-    std::unique_ptr<typename BST<Key, Value>::Node> h) {
+    std::unique_ptr<typename BST<Key, Value>::Node> &h) {
     auto x = std::move(h->right);
     h->right = std::move(x->left);
     static_cast<Node *>(x.get())->color = static_cast<Node *>(h.get())->color;
@@ -55,7 +55,7 @@ std::unique_ptr<typename BST<Key, Value>::Node> RedBlackBST<Key, Value>::rotateL
 
 template<typename Key, typename Value>
 std::unique_ptr<typename BST<Key, Value>::Node> RedBlackBST<Key, Value>::rotateRight(
-    std::unique_ptr<typename BST<Key, Value>::Node> h) {
+    std::unique_ptr<typename BST<Key, Value>::Node> &h) {
     auto x = std::move(h->left);
     h->left = std::move(x->right);
     static_cast<Node *>(x.get())->color = static_cast<Node *>(h.get())->color;
@@ -75,18 +75,18 @@ void RedBlackBST<Key, Value>::flipColors(typename BST<Key, Value>::Node *h) {
 
 template<typename Key, typename Value>
 std::unique_ptr<typename BST<Key, Value>::Node> RedBlackBST<Key, Value>::put(
-    std::unique_ptr<typename BST<Key, Value>::Node> h, const Key &key, const Value &val) {
+    std::unique_ptr<typename BST<Key, Value>::Node> &h, const Key &key, const Value &val) {
     if (!h) return std::make_unique<Node>(key, val, 1, RED);
-    if (key < h->key) h->left = put(std::move(h->left), key, val);
-    else if (key > h->key) h->right = put(std::move(h->right), key, val);
+    if (key < h->key) h->left = put(h->left, key, val);
+    else if (key > h->key) h->right = put(h->right, key, val);
     else h->val = val;
 
-    if (isRed(h->right.get()) && !isRed(h->left.get())) h = rotateLeft(std::move(h));
-    if (isRed(h->left.get()) && isRed(h->left->left.get())) h = rotateRight(std::move(h));
+    if (isRed(h->right.get()) && !isRed(h->left.get())) h = rotateLeft(h);
+    if (isRed(h->left.get()) && isRed(h->left->left.get())) h = rotateRight(h);
     if (isRed(h->left.get()) && isRed(h->right.get())) flipColors(h.get());
 
     h->N = this->size(h->left.get()) + this->size(h->right.get()) + 1;
-    return h;
+    return std::move(h);
 }
 
 template<typename Key, typename Value>
@@ -97,7 +97,7 @@ int RedBlackBST<Key, Value>::height(typename BST<Key, Value>::Node *h) const {
 
 template<typename Key, typename Value>
 void RedBlackBST<Key, Value>::put(const Key &key, const Value &val) {
-    this->root = put(std::move(this->root), key, val);
+    this->root = put(this->root, key, val);
     static_cast<Node *>(this->root.get())->color = BLACK;
 }
 
