7d3fbc058986e915bff40b1f4adb21128ab5ac7e
landerrosette <57791410+landerrosette@users.noreply.github.com>
2024-12-28 08:56:44 +0800

    Refactor remove method to pass reference to std::unique_ptr

--- SequentialSearchST.h
+++ SequentialSearchST.h
@@ -19,14 +19,14 @@ private:
     int N = 0;
     std::unique_ptr<Node> first;
 
-    std::unique_ptr<Node> remove(std::unique_ptr<Node> x, const Key &key);
+    std::unique_ptr<Node> remove(std::unique_ptr<Node> &x, const Key &key);
 
 public:
     std::optional<Value> get(const Key &key) const override;
 
     void put(const Key &key, const Value &val) override;
 
-    void remove(const Key &key) override { first = remove(std::move(first), key); }
+    void remove(const Key &key) override { first = remove(first, key); }
 
     int size() const override { return N; }
 
@@ -35,19 +35,19 @@ public:
 
 template<typename Key, typename Value>
 std::unique_ptr<typename SequentialSearchST<Key, Value>::Node> SequentialSearchST<Key, Value>::remove(
-    std::unique_ptr<Node> x, const Key &key) {
+    std::unique_ptr<Node> &x, const Key &key) {
     if (!x) return nullptr;
     if (key == x->key) {
         --N;
         return std::move(x->next);
     }
-    x->next = remove(std::move(x->next), key);
-    return x;
+    x->next = remove(x->next, key);
+    return std::move(x);
 }
 
 template<typename Key, typename Value>
 std::optional<Value> SequentialSearchST<Key, Value>::get(const Key &key) const {
-    for (Node *x = first.get(); x; x = x->next.get()) {
+    for (const Node *x = first.get(); x; x = x->next.get()) {
         if (key == x->key) return x->val;
     }
     return std::nullopt;
@@ -68,7 +68,7 @@ void SequentialSearchST<Key, Value>::put(const Key &key, const Value &val) {
 template<typename Key, typename Value>
 std::list<Key> SequentialSearchST<Key, Value>::keys() const {
     std::list<Key> queue;
-    for (Node *x = first.get(); x; x = x->next.get()) queue.push_back(x->key);
+    for (const Node *x = first.get(); x; x = x->next.get()) queue.push_back(x->key);
     return queue;
 }
 
