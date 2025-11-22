eacdfce9be8a1ec3a0dcc970756c46ab1226635a
landerrosette <57791410+landerrosette@users.noreply.github.com>
2025-11-22 16:05:21 +0800

    refactor: give uninstantiatable base classes protected initializer

--- algorithms/PQBase.h
+++ algorithms/PQBase.h
@@ -13,6 +13,9 @@ namespace algs4 {
         std::vector<std::optional<Key> > pq; // heap-ordered complete binary tree
         int N = 0;                           // in pq[1..N] with pq[0] unused
 
+        explicit PQBase(int maxN) : pq(maxN + 1) {}
+        ~PQBase() = default;
+
         bool less(int i, int j) { return Compare()(*pq[i], *pq[j]); }
         void exch(int i, int j) { std::swap(pq[i], pq[j]); }
         void swim(int k);
@@ -20,46 +23,46 @@ namespace algs4 {
         std::optional<Key> delTop();
 
     public:
-        explicit PQBase(int maxN) : pq(maxN + 1) {}
-
         bool isEmpty() const { return N == 0; }
         int size() const { return N; }
         void insert(const Key &v);
     };
+}
 
-    template<typename Key, typename Compare>
-    void PQBase<Key, Compare>::swim(int k) {
-        while (k > 1 && less(k / 2, k)) {
-            exch(k / 2, k);
-            k = k / 2;
-        }
+template<typename Key, typename Compare>
+void algs4::PQBase<Key, Compare>::swim(int k) {
+    while (k > 1 && less(k / 2, k)) {
+        exch(k / 2, k);
+        k = k / 2;
     }
+}
 
-    template<typename Key, typename Compare>
-    void PQBase<Key, Compare>::sink(int k) {
-        while (2 * k <= N) {
-            int j = 2 * k;
-            if (j < N && less(j, j + 1)) ++j;
-            if (!less(k, j)) break;
-            exch(k, j);
-            k = j;
-        }
+template<typename Key, typename Compare>
+void algs4::PQBase<Key, Compare>::sink(int k) {
+    while (2 * k <= N) {
+        int j = 2 * k;
+        if (j < N && less(j, j + 1))
+            ++j;
+        if (!less(k, j))
+            break;
+        exch(k, j);
+        k = j;
     }
+}
 
-    template<typename Key, typename Compare>
-    std::optional<Key> PQBase<Key, Compare>::delTop() {
-        auto top = pq[1];
-        exch(1, N--);
-        pq[N + 1] = std::nullopt;
-        sink(1);
-        return top;
-    }
+template<typename Key, typename Compare>
+std::optional<Key> algs4::PQBase<Key, Compare>::delTop() {
+    auto top = pq[1];
+    exch(1, N--);
+    pq[N + 1] = std::nullopt;
+    sink(1);
+    return top;
+}
 
-    template<typename Key, typename Compare>
-    void PQBase<Key, Compare>::insert(const Key &v) {
-        pq[++N] = v;
-        swim(N);
-    }
+template<typename Key, typename Compare>
+void algs4::PQBase<Key, Compare>::insert(const Key &v) {
+    pq[++N] = v;
+    swim(N);
 }
 
 
