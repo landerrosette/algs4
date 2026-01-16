4805fb0d22ac7b8e58b25d787b3e56392633e624
landerrosette <57791410+landerrosette@users.noreply.github.com>
2026-01-17 04:31:23 +0800

    chore: add copyright notice

--- clients/GREP.cpp
+++ clients/GREP.cpp
@@ -1,19 +1,35 @@
+/*
+ * Copyright (C) 2024 landerrosette
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 /******************************************************************************
- *  % more tinyL.txt
- *  AC
- *  AD
- *  AAA
- *  ABD
- *  ADD
- *  BCD
- *  ABCCBD
- *  BABAAA
- *  BABBAAA
+ * % more tinyL.txt
+ * AC
+ * AD
+ * AAA
+ * ABD
+ * ADD
+ * BCD
+ * ABCCBD
+ * BABAAA
+ * BABBAAA
  *
- *  % ./GREP "(A*B|AC)D" < tinyL.txt
- *  ABD
- *  ABCCBD
- *
+ * % ./GREP "(A*B|AC)D" < tinyL.txt
+ * ABD
+ * ABCCBD
  ******************************************************************************/
 
 #include <iostream>
